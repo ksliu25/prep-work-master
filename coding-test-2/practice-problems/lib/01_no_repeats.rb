26ab87a7f6542abfbef62b1c93a28fee32cb1ce4
ksliu25 <ksliu25@gmail.com>
2015-02-08 15:35:41 -0600

    Solved the 2nd one!!
    Though I wonder if there's a better method for no_repeat??

--- coding-test-2/practice-problems/lib/01_no_repeats.rb
+++ coding-test-2/practice-problems/lib/01_no_repeats.rb
@@ -1,2 +1,13 @@
+def no_repeat?(year)
+	s_arr = year.to_s.split("")
+	s_arr.length == s_arr.uniq.length
+end
+
+	
+
 def no_repeats(year_start, year_end)
+	arr = []
+	years = (year_start..year_end).to_a
+	years.each {|x| arr << x if no_repeat?(x)}
+	arr
 end
