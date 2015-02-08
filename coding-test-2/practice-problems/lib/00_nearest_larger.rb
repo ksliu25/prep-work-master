dc2efa07dd2c5290e18229e1cf9a0ed95c4c1124
ksliu25 <ksliu25@gmail.com>
2015-02-08 14:45:34 -0600

    Solved the first one!
    Though, it took awhile and I don't know how I feel about all my nested
    if functions... but at least it works....
    
    I'll take a look at the solution to see how  much more elegant it is.

--- coding-test-2/practice-problems/lib/00_nearest_larger.rb
+++ coding-test-2/practice-problems/lib/00_nearest_larger.rb
@@ -1,2 +1,15 @@
 def nearest_larger(arr, idx)
+	index = 0
+	larger = nil
+	while index < arr.length
+		if arr[index] > arr[idx] 
+			if larger == nil || arr[index] > arr[larger]
+				if larger == nil || idx > larger && idx > index
+					larger = arr.index(arr[index])
+				end
+			end
+		end
+		index += 1
+	end
+	larger
 end
