2d6c061219557b0ce3a680225d411b1ecbf99963
ksliu25 <ksliu25@gmail.com>
2015-02-09 20:39:17 -0600

    Couldn't solve the 3rd one... but good that I get to see each_char and unless in action
    each_char and unless eh? John Nunemaker has a nice write up on unless
    that I looked up.

--- coding-test-2/practice-problems/lib/02_letter_count.rb
+++ coding-test-2/practice-problems/lib/02_letter_count.rb
@@ -1,2 +1,19 @@
+=begin
+
+I had to look at the solution, but I am happy to learn 
+how to use each_char and unless for the first time!
+
+=end
+
+
 def letter_count(str)
+	letter_hash = {}
+	
+	str.each_char do |char|
+		next if char == " "
+		letter_hash[char] = 0 unless letter_hash.include?(char)
+		letter_hash[char] += 1
+	end
+	letter_hash
+	
 end
