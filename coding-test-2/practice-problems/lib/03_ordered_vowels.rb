0cacd42b8a3e016cfcae3df4e80d002baa20fbeb
ksliu25 <ksliu25@gmail.com>
2015-02-09 21:14:25 -0600

    I think I'll use the method I made and make a separate one!!

--- coding-test-2/practice-problems/lib/03_ordered_vowels.rb
+++ coding-test-2/practice-problems/lib/03_ordered_vowels.rb
@@ -1,2 +1,15 @@
 def ordered_vowel_words(str)
+	vowel = nil
+	idx = 0
+	while str.length > idx
+		if str[idx] =~ /[aeiou]/
+			if vowel == nil || str[idx] > vowel || str[idx] == vowel
+				vowel = str[idx]
+				else
+					return ""
+			end
+		end
+		idx += 1
+	end
+	str
 end
