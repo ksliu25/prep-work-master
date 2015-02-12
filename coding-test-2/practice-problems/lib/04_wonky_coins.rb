38be68cf9a5aca943b4521c96dd1e796219f4224
ksliu25 <ksliu25@gmail.com>
2015-02-12 00:20:48 -0600

    I know this requires recursion...
    I just don't know how...

--- coding-test-2/practice-problems/lib/04_wonky_coins.rb
+++ coding-test-2/practice-problems/lib/04_wonky_coins.rb
@@ -1,4 +1,12 @@
 def wonky_coins(n)
-	#flatten method
+	arr = []
+	arr << n/2 << n/3 << n/4
+	#base case?
+	if arr.all?{|coin| coin == 0}
+		return arr.flatten.length
+		else
+		arr.map{|x| wonky_coins(x)}
+	end
 end
 
+p wonky_coins(5)
