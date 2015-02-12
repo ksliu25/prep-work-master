6d1d057200b170c1ec943e25d3b23078d2dd0f17
ksliu25 <ksliu25@gmail.com>
2015-02-12 09:33:28 -0600

    Wow, a much simpler solution

--- coding-test-2/practice-problems/lib/04_wonky_coins.rb
+++ coding-test-2/practice-problems/lib/04_wonky_coins.rb
@@ -1,3 +1,5 @@
+=begin
+RIDICULOUS.
 def wonky_coins(n)
 	arr = []
 	arr << n/2 << n/3 << n/4
@@ -5,8 +7,12 @@ def wonky_coins(n)
 	if arr.all?{|coin| coin == 0}
 		return arr.flatten.length
 		else
-		arr.map{|x| wonky_coins(x)}
+ 		arr.map{|x| wonky_coins(x)}
 	end
 end
+=end
 
-p wonky_coins(5)
+def wonky_coins(n)
+	return 1 if n == 0
+	return wonky_coins(n/2) + wonky_coins(n/3) + wonky_coins(n/4)
+end
