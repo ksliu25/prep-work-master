9aef88732399eebedd1f9c56c4c21d27c4c79193
ksliu25 <ksliu25@gmail.com>
2015-02-14 02:03:18 -0600

    I'll have to come back to this later...
    Wonky_coins is confusing

--- coding-test-2/practice-problems/lib/04_wonky_coins.rb
+++ coding-test-2/practice-problems/lib/04_wonky_coins.rb
@@ -1,18 +1,21 @@
-=begin
-RIDICULOUS.
+
 def wonky_coins(n)
+	return 0 if n == 0
 	arr = []
 	arr << n/2 << n/3 << n/4
 	#base case?
-	if arr.all?{|coin| coin == 0}
-		return arr.flatten.length
+	if n != 0
+		arr.map{|x| wonky_coins(x)}
 		else
- 		arr.map{|x| wonky_coins(x)}
-	end
-end
-=end
+	
 
+end
+
+p wonky_coins(5)
+
+=begin
 def wonky_coins(n)
 	return 1 if n == 0
 	return wonky_coins(n/2) + wonky_coins(n/3) + wonky_coins(n/4)
 end
+=end
